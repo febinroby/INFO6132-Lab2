5e2a2dbc909c5cad13d5a5a710b4e0deeea3be4b
febinroby <febinroby@gmail.com>
2024-10-27 22:00:51 -0400

    Added import for Alerts

--- src/screens/NewTransactionScreen.js
+++ src/screens/NewTransactionScreen.js
@@ -1,5 +1,5 @@
 import React, { useState, useContext } from 'react';
-import { View, Text, TextInput, Button } from 'react-native';
+import { View, Text, TextInput, Button, Alert } from 'react-native';
 import { TransactionsContext } from '../context/TransactionsContext';
 
 const NewTransactionScreen = () => {
